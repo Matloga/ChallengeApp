609e5f865e9b68b0005670e97e0148302e378278
Tsumbedzo Matloga <tsumbedzomatloga@gmail.com>
2026-02-16 16:09:40 +0200

    first phase

---
+++ src/main/java/com/matloga/ChallengeApp/ChallengeService.java
@@ -0,0 +1,44 @@
+package com.matloga.ChallengeApp;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChallengeService {
+    private List<Challenge> challenges = new ArrayList<>();
+
+    private Long nextId = 1L;
+
+    public ChallengeService(){
+    }
+
+    public List<Challenge> getChallenges(){
+        return challenges;
+    }
+
+    public boolean addChallenge(Challenge challenge) {
+        if (challenge != null) {
+            challenge.setId(nextId++);
+            challenges.add(challenge);
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    public Challenge getChallenge(String month) {
+            for (Challenge challenge : challenges) {
+                if (challenge.getMonth().equalsIgnoreCase(month)) {
+                    return challenge;
+                }
+            }
+            return null;
+    }
+
+    public List<Challenge> getAllChallenges() {
+        return challenges;
+    }
+}
